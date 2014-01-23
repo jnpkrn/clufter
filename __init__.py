e5137c7a985adfad0ba6aea805030973da3e04ca
Jan Pokorný <jpokorny@redhat.com>
2014-01-23 21:01:23 +0100

    __init__: s/version_line/version_text/ + add description{,_text}
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- __init__.py
+++ __init__.py
@@ -8,10 +8,33 @@ author = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
 
 metadata = (version, copyright, author)
 
+description = """\
+Tool to perform various transformations on cluster configuration formats.
 
-def version_line(package='clufter', sep=', '):
-    return (sep.join((package,) + metadata)
+Primarily aimed at (CMAN, rgmanager) -> (Corosync, Pacemaker) cluster
+stacks configuration conversion (following RHEL trend), the tool,
+however, offers more uses further extensible within the arranged
+commands-filters-formats plugin framework.
+"""
+#Native plugins allows also for obfuscation of sensitive data within
+#cluster configuration of supported cluster stacks, and other
+#convenient actions.
+
+
+def version_text(package=None, sep='\n'):
+    if package is None:
+        from os.path import basename, dirname
+        package = basename(dirname(__file__))
+    return (package + ' ' + sep.join(metadata)
             .replace(' \n', '\n')
             .replace(' @at@ ', '@')
             .replace(' .dot. ', '.')
             .replace('@Red Hat.', '@redhat.'))
+
+
+def description_text(width=70):
+    if not width:
+        return description
+    from textwrap import fill
+    desc = map(lambda p: p.replace('\n', ' '), description.split('\n\n'))
+    return '\n\n'.join(map(lambda p: fill(p, width), desc)) + '\n'
