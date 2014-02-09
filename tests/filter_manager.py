ff7d17137f6566e1b904f115664b24ffdf2be472
Jan Pokorný <jpokorny@redhat.com>
2014-02-09 23:04:58 +0100

    tests: filter_manager: test CompositeFormat-based filter
    ...which is internally, in fact, just a doubled-proceeding of
    ccs2ccsflat filter
    
    also:
    - start describing tests using CHECK "keyword" in the comments
    - fix borken assumption wrt. equality of module-level import
      and the recycled auto-discovered plugin
    
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- tests/filter_manager.py
+++ tests/filter_manager.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2013 Red Hat, Inc.
+# Copyright 2014 Red Hat, Inc.
 # Part of clufter project
 # Licensed under GPLv2 (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
 """Testing filter manager"""
@@ -11,9 +11,9 @@ from os.path import dirname, join
 import _bootstrap  # known W402, required
 
 from clufter.format_manager import FormatManager
+from clufter.filter import Filter
 from clufter.filter_manager import FilterManager
-from clufter.filters.ccs2ccsflat import ccs2ccsflat
-from clufter.filters.ccsflat2pcs import ccsflat2pcs
+from clufter.utils import head_tail
 
 
 class FilterManagerTestCase(unittest.TestCase):
@@ -27,18 +27,66 @@ class FilterManagerTestCase(unittest.TestCase):
 
 class Default(FilterManagerTestCase):
     def test_default(self):
+        # NOTE imports has to be just there due to environment changed
+        #      by "starting from scratch" + plugin discovery elsewhere
+        from clufter.filters.ccs2ccsflat import ccs2ccsflat
+        from clufter.filters.ccsflat2pcs import ccsflat2pcs
         filters = self.flt_mgr.filters
         #print filters
         for cls in ccs2ccsflat, ccsflat2pcs:
+            # CHECK selected built-in plugin is auto-discovered
             self.assertTrue(cls.name in filters)
-            self.assertEqual(cls, type(filters[cls.name]))
+            # CHECK it's class matches the class of natively imported one
+            #       during this test-run (not generally due to "restart")
+            self.assertIs(cls, type(filters[cls.name]))
 
     def test_run_ccs2ccsflat(self):
-        # using ./empty.conf
         testfile = join(dirname(__file__), 'empty.conf')
+        # CHECK the auto-discovered filter properly tracked
         self.assertTrue('ccs2ccsflat' in self.flt_mgr.filters)
         out_obj = self.flt_mgr('ccs2ccsflat', ('file', testfile))
-        # XXX print out_obj('bytestring')
+        result = out_obj('bytestring')
+        # XXX print result
+        # CHECK the externalized representation matches the original
+        with file(testfile) as f:
+            self.assertEqual(result, f.read())
+
+
+class CompositeFormatIO(FilterManagerTestCase):
+    """Exercising filters with composite formats"""
+    def setUp(self):
+        @Filter.deco(('ccs', 'ccs'), ('ccsflat', 'ccsflat'))
+        def double_ccs2ccsflat(self, in_objs, flt_ctxt, verify=False):
+            from clufter.filters.ccs2ccsflat import ccs2ccsflat
+            from clufter.formats.ccs import ccs, ccsflat
+            ccs2ccsflat = ccs2ccsflat(ccs, ccsflat)
+            outs = []
+            for in_obj in in_objs:
+                outs.append(('bytestring', ccs2ccsflat(in_obj)('bytestring')))
+            subprotos, subresults = head_tail(zip(*outs))
+            return ('composite', subprotos), subresults
+        super(CompositeFormatIO, self).setUp()
+
+    def test_run_double_ccs2ccsflat(self):
+        testfile = join(dirname(__file__), 'empty.conf')
+        # CHECK the filter defined in setUp method properly tracked
+        self.assertTrue('double_ccs2ccsflat' in self.flt_mgr.filters)
+        # perform the filter
+        out_objs = self.flt_mgr('double_ccs2ccsflat',
+            (('composite', ('file', 'file')), testfile, testfile)
+        )
+        # CHECK resulting objects are not identical (separate processing)
+        # NOTE there could be heuristic to capture duplicated work, but such
+        #      filter is phony anyway
+        self.assertIsNot(*out_objs)
+        # externalize outputs
+        results = out_objs(('composite', ('bytestring', 'bytestring')))
+        # XXX print results
+        # CHECK resulting externalized reprezentation is, however, tha same
+        self.assertEqual(*results)
+        # CHECK picked externalized representation matches the original
+        with file(testfile) as f:
+            self.assertEqual(results[0], f.read())
 
 
 if __name__ == '__main__':
