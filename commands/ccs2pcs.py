2c1615c349f3ab69cd671fbaa7628e9eaa944ace
Jan Pokorný <jpokorny@redhat.com>
2014-02-13 21:50:44 +0100

    commands:ccs2pcs: invert opt. 'validate'->'nocheck' (def.:yes->no)
    due to previous think as boolean opts. cannot be passed specifying value
    
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- commands/ccs2pcs.py
+++ commands/ccs2pcs.py
@@ -17,7 +17,7 @@ def ccs2pcs(cmd_ctxt,
             input='/etc/cluster/cluster.conf',
             output='./cib.xml',
             coro='./corosync.conf',
-            validate=True):
+            nocheck=False):
     """Converts cman-based cluster configuration to Pacemaker-based one
 
     There are two outputs: Pacemaker configuration ala cib.xml and
@@ -27,8 +27,8 @@ def ccs2pcs(cmd_ctxt,
         input      input cman-based cluster configuration file
         output     output pacemaker-based configuration file
         coro       output Corosync configuration file
-        validate   whether to validate each step when self-checks available
+        nocheck    do not validate any step (even if self-checks present)
     """
-    #cmd_ctxt.filter()['validate'] = validate
-    #cmd_ctxt.filter('ccs2ccsflat')['validate'] = validate
+    #cmd_ctxt.filter()['validate'] = not nocheck
+    #cmd_ctxt.filter('ccs2ccsflat')['validate'] = not nocheck
     return ('file', input), (('file', output), ('file', coro))
