7c9f5265c5798af36ae966c34a4e19d3c7895db7
Jan Pokorný <jpokorny@redhat.com>
2014-02-13 19:19:41 +0100

    command_context: fix discovered issues in the proxying
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- command_context.py
+++ command_context.py
@@ -33,7 +33,10 @@ class CommandContextBase(MutableMapping):
 
     def __getitem__(self, key):
         #return self._dict[key]
-        return self._dict.get(key)  # make it soft-error
+        return self._dict.get(key)  # make it soft-error (->setdefault reimpl.)
+
+    def setdefault(self, key, default=None):
+        return self._dict.setdefault(key, default)
 
     def __iter__(self):
         return iter(self._dict)
@@ -71,7 +74,10 @@ class CommandContext(CommandContextBase):
                             lambda this, *args, **kwargs: \
                                 ret(this, this, *args, **kwargs)
                 else:
-                    ret = obj.__getattribute__(name)
+                    try:
+                        ret = super(wrapped, self).__getattribute__(name)
+                    except AttributeError:
+                        ret = obj.__getattribute__(name)
                 return ret
 
             def __setattribute__(self, name, value):
