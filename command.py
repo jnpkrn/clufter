c40ec3b0f74e380c2a1e054473f2878d6c10a7ec
Jan Pokorný <jpokorny@redhat.com>
2014-02-13 20:22:24 +0100

    command: add basic IO chain analysis
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- command.py
+++ command.py
@@ -19,6 +19,8 @@ from .utils import args2sgpl, \
                    func_defaults_varnames, \
                    head_tail, \
                    hybridproperty, \
+                   selfaware, \
+                   tuplist, \
                    zip_empty
 
 log = logging.getLogger(__name__)
@@ -54,6 +56,88 @@ class Command(object):
         # of them will be run later on
         self._desc_opts = None
         self._fnc_defaults_varnames = None
+        self._filter_chain_analysis = None  # will be dict
+
+    #
+    # filter chain related
+    #
+
+    @property
+    def filter_chain_analysis(self):
+        if self._filter_chain_analysis is None:
+            filter_chain = self._filter_chain
+            self._filter_chain_analysis = self.analyse_chain(filter_chain)
+        return self._filter_chain_analysis
+
+    @staticmethod
+    @selfaware
+    def analyse_chain(me, filter_chain, analysis_acc=None):
+        """Given the filter chain, return filter backtrack and terminal chain
+
+        This is done by recursive traversal.  Also check that the graph is
+        actually connected wrt. protocols compatibility between each of
+        adjacent filters is performed.
+
+        XXX: mentioned check doesn't know about CompositeFormat and
+             the connected magic, yet
+        """
+        new = analysis_acc is None
+        if new:
+            analysis_acc = dict(filter_backtrack={},
+                                terminal_chain=[[]])
+        filter_backtrack = analysis_acc['filter_backtrack']
+        terminal_chain = analysis_acc['terminal_chain'][-1]
+
+        assert tuplist(filter_chain)
+        # PASSDOWN or FILTERS
+        pass_through, filter_chain = head_tail(*filter_chain) \
+                                     if isinstance(filter_chain, tuple) \
+                                     and len(filter_chain) > 1 \
+                                     else (None, filter_chain)
+        for i in filter_chain:
+            i, i_tail = head_tail(i)
+            bt = filter_backtrack.setdefault(i, {})
+            if new or not (bt or i_tail):
+                # new for UPFILTERs, which are also terminals (input ones)
+                terminal_chain.append(i)
+            if pass_through:
+                if pass_through in bt:
+                    raise CommandError(me,
+                        "filter `{0}' is feeded by `{1}' more than once",
+                        i.__class__.__name__, pass_through.__class__.__name__
+                    )
+                common_protocols = sorted(
+                    reduce(
+                        set.intersection,
+                        map(set, (pass_through.out_format._protocols,
+                                  i.in_format._protocols))
+                    ),
+                    key=lambda x:
+                        int(x == pass_through.out_format.native_protocol)
+                        + int(x == i.in_format.native_protocol)
+                )
+                if not common_protocols:
+                    raise CommandError(me,
+                        "filter `{0}' and its feeder `{1}' have no protocol"
+                        " in common",
+                        i.__class__.__name__, pass_through.__class__.__name__
+                    )
+                bt[pass_through] = common_protocols
+            if i_tail:
+                # PASSDOWN
+                # this uses a dirty trick of exploiting the end of the list
+                # as a sort of a stack, where the per-recursion-level result
+                # is available for the caller (who is also responsible for
+                # preparing a new list here for callee to fill) so it can
+                # move it to the right position afterwards
+                analysis_acc['terminal_chain'].append([])  # not terminal_chain
+                me((i, ) + i_tail, analysis_acc)
+                terminal_chain.append(analysis_acc['terminal_chain'].pop())
+            elif new:
+                # yes, terminal UPFILTER is tracked twice as terminal (I/O)
+                terminal_chain.append(i)
+
+        return analysis_acc
 
     #
     # self-introspection (arguments, description, options)
