c72f6255f1768fe9c38fe7c156a1e68a6fac506d
Jan Pokorný <jpokorny@redhat.com>
2014-04-01 23:22:17 +0200

    tests/command: sanitize/simplify, make it passing (incl. el6)
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- tests/command.py
+++ tests/command.py
@@ -7,7 +7,7 @@ __author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
 
 import unittest
 from os.path import dirname, join
-from os import remove
+from os import remove, stat
 #from pprint import pprint
 
 import _bootstrap  # known W402, required
@@ -38,40 +38,78 @@ class ChainResolve(unittest.TestCase):
         testoutput = mktemp(prefix='out', suffix='.conf',
                             dir=join(dirname(__file__), 'tmp'))
 
-        #@Command.deco(('ccs2ccsflat',
-        #               'ccsflat2pcs', 'ccs2coroxml'))
-        #def cmd_chain_match_01(cmd_ctxt,
-        #                    input=testfile, output=testoutput, coro='.coro'):
-        #    return ('file', input), (('file', output), ('file', coro))
-        #@Command.deco(('ccs2ccsflat'))
-        #def cmd_chain_match_02(cmd_ctxt,
-        #                    input=testfile, output=testoutput, coro='.coro'):
-        #    return ('file', input), ('file', output)
+        @Command.deco(('ccs2ccsflat',
+                          ('ccsflat2pcs'),
+                          ('ccs2coroxml')))
+        def cmd_chain_match_01(cmd_ctxt,
+                               input=testfile,
+                               output=testoutput,
+                               coro='.coro'):
+            return (
+                ('file', input),
+                (
+                    ('file', output),
+                    ('file', coro)
+                )
+            )
+        @Command.deco(('ccs2ccsflat'))
+        def cmd_chain_match_02(cmd_ctxt,
+                               input=testfile,
+                               output=testoutput,
+                               coro='.coro'):
+            return (
+                ('file', input),
+                ('file', output)
+            )
         @Command.deco(('ccs2ccsflat'))
         def cmd_chain_nonmatch_01(cmd_ctxt,
-                                  input=testfile, output=testoutput,
+                                  input=testfile,
+                                  output=testoutput,
                                   coro='.coro'):
-            return ('file', input), (('file', output), ('file', coro))
+            return (
+                ('file', input),
+                (
+                    ('file', output),
+                    ('file', coro)
+                )
+            )
         @Command.deco(('ccs2ccsflat',
-                       'ccsflat2pcs', 'ccs2coroxml', 'ccs2coroxml'))
+                          ('ccsflat2pcs'),
+                          ('ccs2coroxml'),
+                          ('ccs2coroxml')))
         def cmd_chain_nonmatch_02(cmd_ctxt,
-                                  input=testfile, output=testoutput,
+                                  input=testfile,
+                                  output=testoutput,
                                   coro='.coro'):
-            return ('file', input), (('file', output), ('file', coro))
+            return (
+                ('file', input),
+                (
+                    ('file', output),
+                    ('file', coro)
+                )
+            )
         # malformed protocol name
         @Command.deco(('ccs2ccsflat',
-                       'ccsflat2pcs', 'ccs2coroxml'))
+                          ('ccsflat2pcs'),
+                          ('ccs2coroxml')))
         def cmd_chain_nonmatch_03(cmd_ctxt,
-                                  input=testfile, output=testoutput,
+                                  input=testfile,
+                                  output=testoutput,
                                   coro='.coro'):
-            return ('file', input), (('file', output), ('life', coro))
-
-        cmd_classes = (#cmd_chain_match_01,
-                       #cmd_chain_match_02,
-                       cmd_chain_nonmatch_01,
-                       cmd_chain_nonmatch_02,
-                       cmd_chain_nonmatch_03
-                       )
+            return (
+                ('file', input),
+                (
+                    ('file', output),
+                    ('life', coro)
+                )
+            )
+        cmd_classes = (
+            cmd_chain_match_01,
+            cmd_chain_match_02,
+            cmd_chain_nonmatch_01,
+            cmd_chain_nonmatch_02,
+            cmd_chain_nonmatch_03
+        )
         split = cmd_classes.index(cmd_chain_nonmatch_01)
         for i, cmd_cls in enumerate(cmd_classes):
             res_input = cmd_cls.filter_chain
@@ -80,24 +118,18 @@ class ChainResolve(unittest.TestCase):
                 #log.debug("resolve at `{0}' command: `{1}' -> {2}"
                 #          .format(cmd_cls.name, repr(res_input), repr(res_output)))
                 try:
-                    cmd = cmd_cls(*res_output)
-                    cmd({}, [])
+                    cmd_cls(*res_output)({}, [])  # no args/kwargs
                 except CommandError as e:
                     print "{0}: {1}".format(cmd_cls.name, e)
                     self.assertFalse(i < split)
                 except Exception as e:
                     print "{0}: {1}".format(cmd_cls.name, e)
+                    self.assertTrue(i < split)
                     raise
-                    self.assertTrue(i < split)
                 else:
                     self.assertTrue(i < split)
-                try:
-                    with open(testfile) as src, open(testoutput) as dst:
-                        #self.assertEqual(src.read(), dst.read())
-                        pass
-                except IOError:
-                    pass
-                else:
+                    # also test non-zero-size output whe
+                    self.assertTrue(stat(testoutput).st_size > 0)
                     remove(testoutput)
                 continue
             self.assertTrue(False)
