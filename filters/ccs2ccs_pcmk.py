164ca85c3877f208efe98a72a3985a7f3ecaa09f
Jan Pokorný <jpokorny@redhat.com>
2014-04-04 16:19:47 +0200

    filters/ccs2ccs_pcmk: comment wrt. possible static output v2
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- filters/ccs2ccs_pcmk.py
+++ filters/ccs2ccs_pcmk.py
@@ -11,5 +11,8 @@ from clufter.filter import XMLFilter
 @XMLFilter.deco('ccs', 'ccs')
 def ccs2ccs_pcmk(flt_ctxt, in_obj):
     # could be static output, but stay with XSLT in case we want to move sth.
-    # from cluster.conf to corosync.conf already (used to be a case)
+    # from cluster.conf to corosync.conf already (used to be a case);
+    # there is also a possibility to put service-only directive
+    # as a file under /etc/corosync/service.d/ (called, e.g., pcmk,
+    # see Clusters from Scratch document)
     return ('etree', flt_ctxt.proceed_xslt_filter(in_obj, raw=flt_ctxt['raw']))
