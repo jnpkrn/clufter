10fd4fe1e1532a0a85ce56e8baa368dc21dc40ef
Jan Pokorný <jpokorny@redhat.com>
2014-04-17 21:46:11 +0200

    filters/pcs2simplepcs: strip fencing-topology if empty
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

---
+++ filters/cib/configuration/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2014 Red Hat, Inc.
+# Part of clufter project
+# Licensed under GPLv2 (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
+__author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
+
+
+pcs2simplepcs = '''\
+    <clufter:descent-mix preserve-rest="true"/>
+    <!-- strip empty optional elements -->
+    <xsl:template match="fencing-topology[count(*) = 0]"/>
+'''
