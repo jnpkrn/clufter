f6046694c11a61ed0e678ca7f8b8d32eaf24ada3
Jan Pokorný <jpokorny@redhat.com>
2014-06-06 18:38:46 +0200

    runtests.sh: add auto-paging option: -p
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Copyright 2013 Red Hat, Inc.                                                
+# Copyright 2014 Red Hat, Inc.
 # Part of clufter project                                                     
 # Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
 
@@ -13,7 +13,7 @@ stop=$(printf "\033\[0m")
 blue=$(printf "\033[01;34m") red=$(printf "\033[01;31m")
 green=$(printf "\033[01;32m") cyan=$(printf "\033[01;36m")
 magenta=$(printf "\033[01;35m")
-COLORIZE='|& sed \
+TAIL='|& sed \
     -e "s/\(^\|[^A-Za-z]\)\(OK\)/\1${blue}\2${stop}/"      \
     -e "s/\(^\|[^A-Za-z]\)\(FAILED.*\)/\1${red}\2${stop}/" \
     -e "s/\(^\|[^A-Za-z]\)\(ok\)/\1${green}\2${stop}/"     \
@@ -23,21 +23,25 @@ COLORIZE='|& sed \
     -e "s/^\(Ran [1-9][0-9]*.*\)/${cyan}\1${stop}/"'
 
 if python -c "import sys; sys.exit(sys.version_info[:2] < (2,7))"; then
-	CMD="python -m unittest"
+    CMD="python -m unittest"
 else
-	# pre-2.7 unittest doesn't offer test discovery, use external unittest2
-	CMD="unit2"
+    # pre-2.7 unittest doesn't offer test discovery, use external unittest2
+    CMD="unit2"
 fi
 DEBUG="env LOGLEVEL=WARNING"
+PGR=${PAGER:-less}
 VERBOSE=1
 ACC=
 while [ $# -gt 0 ]; do
     case "$1" in
-        "-d")
+        -d)
             DEBUG=
             ;;
-        "-q")
+        -q)
             VERBOSE=0
+            ;;
+        -p)
+            TAIL+=" 2>&1 | ${PGR}"  # bash: " |& ${PGR}"
             ;;
         *)
             ACC+=" $1"
@@ -45,13 +49,13 @@ while [ $# -gt 0 ]; do
     esac
     shift
 done
+if [ ! -t 0 ]; then
+    TAIL=
+fi
 if [ -z "${ACC}" ]; then
     ACC=" discover -s tests -p '*.py'"
-    if [ "$VERBOSE" -eq 1 ]; then
+    if [ "${VERBOSE}" -eq 1 ]; then
         ACC+=" --verbose"
     fi
 fi
-if [ ! -t 0 ]; then
-    COLORIZE=
-fi
-eval "$DEBUG $CMD $ACC $COLORIZE"
+eval "${DEBUG} ${CMD} ${ACC} ${TAIL}"
