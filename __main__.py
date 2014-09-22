9eb637d4d694b5e764b305f5e2ae0869d3dee181
Jan Pokorný <jpokorny@redhat.com>
2014-09-22 19:32:19 +0200

    Expose CLI frontend to be runnable via "python -m" syntax
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

---
+++ __main__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Copyright 2014 Red Hat, Inc.
+# Part of clufter project
+# Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
+"""Machinery entry point -- to be run via python -m <package>"""
+__author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
+
+from os.path import basename
+from sys import argv, exit
+
+from .main import run
+
+if basename(argv[0]) == '__main__.py':
+    argv[0] = 'python -m ' + __package__
+exit(run(argv))
