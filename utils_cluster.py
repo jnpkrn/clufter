3214c812a4992ba83ff605026bcadaec406477cb
Jan Pokorný <jpokorny@redhat.com>
2014-09-25 14:15:26 +0200

    utils_cluster: update knowledge about cluster in Fedora/Ubuntu
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- utils_cluster.py
+++ utils_cluster.py
@@ -33,7 +33,14 @@ cluster_map = {
     'linux':
         {
             'fedora': (
-                ((18, 0), {
+                ((13, 0), {
+                    'corosync':        (1, 3),
+                    'pacemaker[cman]': (1, 1),
+                }),
+                ((14, 0), {
+                    'corosync':        (1, 4),
+                }),
+                ((17, 0), {
                     'corosync':        (2, 3),
                     'pacemaker[coro]': (1, 1),
                 }),
@@ -49,8 +56,19 @@ cluster_map = {
                     'pacemaker[cman]': (1, 1),
                 }),
                 ((7, 0), {
-                    'pacemaker[coro]': (1, 1),
                     'corosync':        (2, 3),
+                    'pacemaker[coro]': (1, 1),
+                }),
+            ),
+            # may be inaccurate
+            'ubuntu': (
+                ((13, 04), {
+                    'corosync':        (1, 4),
+                    'pacemaker[cman]': (1, 1),
+                }),
+                ((13, 10), {
+                    'corosync':        (2, 3),
+                    'pacemaker[coro]': (1, 1),
                 }),
             ),
         },
