cde2f5dec372ea8580d67297aa36ecddbe135981
Jan Pokorný <jpokorny@redhat.com>
2014-10-10 18:21:57 +0200

    filters/ccsflat2pcs: encode services into valid, yet uncomplete CIB
    IOW, there will have to be another pass that will use the data we have
    prepared by known, massage them, and finally (and hopefully) spit out
    the desired full-fledged cib, incl. reasonable constraints arrangement.
    
    Apparently, ./run-check doesn't work as of this commit, but it's
    worthless to edit it just to keep temporarily passing, as the final
    result is different anyway.
    
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

---
+++ filters/cluster/rm/failoverdomains/failoverdomain/__init__.py
@@ -0,0 +1,49 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2014 Red Hat, Inc.
+# Part of clufter project
+# Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
+__author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
+
+###
+
+from .... import package_name
+
+ccsflat2pcs = ('''\
+    <template id="FAILOVERDOMAIN-{@name}"
+              class="ocf"
+              provider="%(package_name)s"
+              type="temporary-failoverdomain">
+        <meta_attributes id="FAILOVERDOMAIN-{@name}-META">
+            <nvpair id="FAILOVERDOMAIN-{@name}-META-ordered"
+                    name="ordered"
+                    value="0">
+                <xsl:if test="contains('123456789',
+                                        substring(@ordered, 1, 1))">
+                    <xsl:attribute name="value">
+                        <xsl:value-of select="'1'"/>
+                    </xsl:attribute>
+                </xsl:if>
+            </nvpair>
+            <nvpair id="FAILOVERDOMAIN-{@name}-META-restricted"
+                    name="restricted"
+                    value="0">
+                <xsl:if test="contains('123456789',
+                                        substring(@restricted, 1, 1))">
+                    <xsl:attribute name="value">
+                        <xsl:value-of select="'1'"/>
+                    </xsl:attribute>
+                </xsl:if>
+            </nvpair>
+            <nvpair id="FAILOVERDOMAIN-{@name}-META-nofailback"
+                    name="nofailback"
+                    value="0">
+                <xsl:if test="contains('123456789',
+                                        substring(@nofailback, 1, 1))">
+                    <xsl:attribute name="value">
+                        <xsl:value-of select="'1'"/>
+                    </xsl:attribute>
+                </xsl:if>
+            </nvpair>
+        </meta_attributes>
+    </template>
+''') % dict(package_name=package_name())
