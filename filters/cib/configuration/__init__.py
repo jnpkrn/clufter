91456b23b54867f17d9477370a38eb7c06495ebb
Jan Pokorný <jpokorny@redhat.com>
2014-10-15 19:26:55 +0200

    filters/pcscompact2pcs[configuration]: disable stonith if reasonable
    Also be verbose about not doing it with a desire (message + comment).
    
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- filters/cib/configuration/__init__.py
+++ filters/cib/configuration/__init__.py
@@ -10,3 +10,35 @@ pcsprelude2pcscompact = '''\
     <!-- strip empty optional elements -->
     <xsl:template match="fencing-topology[count(*) = 0]"/>
 '''
+
+###
+
+pcscompact2pcs = ('''\
+    <xsl:template match="crm_config[
+                             not(
+                                 following-sibling::resources/primitive[
+                                     @class = 'stonith'
+                                 ]|following-sibling::resources/primitive[
+                                     preceding-sibling::template[
+                                         @class = 'stonith'
+                                         and
+                                         @id = current()/@template
+                                     ]
+                                 ]
+                             )
+                         ]">
+        <xsl:copy>
+        <xsl:comment> %(note_stonith)s </xsl:comment>
+        <xsl:message>%(note_stonith)s</xsl:message>
+        <cluster_property_set id="CRMCONFIG-bootstrap">
+            <nvpair id="CRMCONFIG-bootstrap-STONITH-ENABLED"
+                    name="stonith-enabled"
+                    value="false"/>
+        </cluster_property_set>
+        </xsl:copy>
+    </xsl:template>
+''') % dict(
+    note_stonith="NOTE: no fencing is configured hence stonith is disabled;"
+                 " please note, however, that this is suboptimal, especially"
+                 " in shared storage scenarios"
+)
