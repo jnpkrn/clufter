38d9777aa64ef46dcf80eb612b0d18324ef72958
Jan Pokorný <jpokorny@redhat.com>
2014-10-17 21:12:09 +0200

    filters/pcscompact2pcs[configuration]: elaborate on constraints
    Fine-tuning needed + nofailback -> stickiness
    
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- filters/cib/configuration/__init__.py
+++ filters/cib/configuration/__init__.py
@@ -13,6 +13,8 @@ pcsprelude2pcscompact = '''\
 
 ###
 
+from .... import package_name
+
 pcscompact2pcs = ('''\
     <xsl:template match="crm_config[
                              not(
@@ -37,8 +39,139 @@ pcscompact2pcs = ('''\
             </cluster_property_set>
         </xsl:copy>
     </xsl:template>
+
+    <xsl:template match="constraints">
+        <xsl:copy>
+            <xsl:for-each select="../resources/template[
+                                    @provider = '%(package_name)s'
+                                    and
+                                    @type = 'temporary-failoverdomain'
+                                ]">
+                <xsl:variable name="FailoverDomain" select="."/>
+                <xsl:for-each select="../template[
+                                        @provider = '%(package_name)s'
+                                        and
+                                        @type = 'temporary-service'
+                                        and
+                                        meta_attributes/nvpair[
+                                            @name = 'domain'
+                                        ]/@value = $FailoverDomain/@id
+                                    ]">
+                    <xsl:variable name="Service" select="."/>
+                    <xsl:variable name="Resources"
+                                  select="../primitive[
+                                              meta_attributes/nvpair[
+                                                  @name = 'rgmanager-service'
+                                                  and
+                                                  @value = $Service/@id
+                                              ]
+                                          ]
+                                          |
+                                          ../group[
+                                            @id = concat($Service/@id, '-GROUP')
+                                          ]"/>
+                    <xsl:for-each select="$Resources">
+                        <xsl:variable name="Resource" select="."/>
+
+                        <!-- rsc_location ~ failoverdomain/failoverdomainnodes -->
+
+                        <xsl:if test="count(
+                                          $FailoverDomain/meta_attributes/nvpair[
+                                              starts-with(@name, 'failoverdomainnode-')
+                                          ]
+                                      ) != 0">
+                            <rsc_location id="CONSTRAINT-LOCATION-{$Resource/@id}"
+                                          rsc="{$Resource/@id}">
+                                <xsl:comment
+                                ><xsl:value-of select="concat(' mimic failoverdomain (',
+                                                              substring-after($FailoverDomain/@id, 'FAILOVERDOMAIN-'),
+                                                              ') for ', $Service/@id, ' ')"
+                                /></xsl:comment>
+                                <xsl:for-each select="$FailoverDomain/meta_attributes/nvpair[
+                                                          starts-with(@name, 'failoverdomainnode-')
+                                                      ]">
+                                    <rule id="CONSTRAINT-LOCATION-{$Resource/@id}-{@value}">
+                                        <xsl:attribute name="score">
+                                            <xsl:choose>
+                                                <xsl:when test="$FailoverDomain/meta_attributes/nvpair[
+                                                                    @name = 'ordered'
+                                                                ]/@value = 1
+                                                                and
+                                                                number(
+                                                                    substring-after(@name,
+                                                                                    'failoverdomainnode-')
+                                                                ) &gt; 0">
+                                                    <xsl:variable name="Result"
+                                                                  select="(101 - number(
+                                                                              substring-after(@name,
+                                                                                              'failoverdomainnode-')
+                                                                          )) * 10000"/>
+                                                    <xsl:choose>
+                                                        <xsl:when test="$Result = 1000000">
+                                                            <xsl:value-of select="'INFINITY'"/>
+                                                        </xsl:when>
+                                                        <xsl:otherwise>
+                                                            <xsl:value-of select="$Result"/>
+                                                        </xsl:otherwise>
+                                                    </xsl:choose>
+                                                </xsl:when>
+                                                <xsl:when test="$FailoverDomain/meta_attributes/nvpair[
+                                                                    @name = 'ordered'
+                                                                ]/@value = 1">
+                                                    <xsl:value-of select="500000"/>
+                                                </xsl:when>
+                                                <xsl:otherwise>
+                                                    <xsl:value-of select="'INFINITY'"/>
+                                                </xsl:otherwise>
+                                            </xsl:choose>
+                                        </xsl:attribute>
+                                        <expression id="CONSTRAINT-LOCATION-{$Resource/@id}-{@value}-expr"
+                                                    attribute="#uname"
+                                                    operation="eq"
+                                                    value="{@value}"/>
+                                    </rule>
+                                </xsl:for-each>
+                                <xsl:if test="$FailoverDomain/meta_attributes/nvpair[
+                                                  @name = 'restricted'
+                                              ]/@value = 1">
+                                    <xsl:comment
+                                    ><xsl:value-of select="concat(' mimic RESTRICTED failoverdomain (',
+                                                                  substring-after($FailoverDomain/@id, 'FAILOVERDOMAIN-'),
+                                                                  ') for ', $Service/@id, ' ')"
+                                    /></xsl:comment>
+                                    <rule id="CONSTRAINT-LOCATION-{$Resource/@id}-RESTRICTED"
+                                          boolean-op="and"
+                                          score="-INFINITY">
+                                        <xsl:for-each select="$FailoverDomain/meta_attributes/nvpair[
+                                                                  starts-with(@name, 'failoverdomainnode-')
+                                                              ]">
+                                            <expression id="CONSTRAINT-LOCATION-{$Resource/@id}-RESTRICTED-{@value}-expr"
+                                                        attribute="#uname"
+                                                        operation="ne"
+                                                        value="{@value}"/>
+                                        </xsl:for-each>
+                                    </rule>
+                                </xsl:if>
+                            </rsc_location>
+                        </xsl:if>
+                    </xsl:for-each>
+                    <xsl:if test="count($Resources) &gt; 1">
+                        <rsc_order id="CONSTRAINT-ORDER-{$Service/@id}"
+                                score="INFINITY">
+                            <resource_set id="CONSTRAINT-ORDER-{$Service/@id}-RESOURCES">
+                                <xsl:for-each select="$Resources">
+                                    <resource_ref id="{@id}"/>
+                                </xsl:for-each>
+                            </resource_set>
+                        </rsc_order>
+                    </xsl:if>
+                </xsl:for-each>
+            </xsl:for-each>
+        </xsl:copy>
+    </xsl:template>
 ''') % dict(
     note_stonith="NOTE: no fencing is configured hence stonith is disabled;"
                  " please note, however, that this is suboptimal, especially"
-                 " in shared storage scenarios"
+                 " in shared storage scenarios",
+    package_name=package_name()
 )
