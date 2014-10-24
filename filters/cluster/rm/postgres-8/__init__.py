25885469011804c59f6e3e12c7f1a0d1b01141df
Jan Pokorný <jpokorny@redhat.com>
2014-10-24 15:54:47 +0200

    filters/ccsflat2pcsprelude[postgres-8]: fix lost functionality
    (ex-post sanitize "new command/cluster.conf analysis" commit, vol. 2)
    
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- filters/cluster/rm/postgres-8/__init__.py
+++ filters/cluster/rm/postgres-8/__init__.py
@@ -10,3 +10,46 @@ ccs_artefacts = ''.join((
     artefact_cond_ra('@config_file',
                      kind='A', desc='configuration file'),
 ))
+
+###
+
+from ...utils_cib import ResourceSpec
+
+ccsflat2pcsprelude = '''\
+    <!--
+        pgsql ~ postgres-8
+     -->
+    <xsl:when test="name() = 'postgres-8'">
+''' + \
+        ResourceSpec('ocf:heartbeat:pgsql').xsl_attrs \
++ '''
+        <!-- INSTANCE_ATTRIBUTES -->
+        <instance_attributes id="{concat($Prefix, '-ATTRS')}">
+            <!-- config ~ config_file -->
+            <nvpair id="{concat($Prefix, '-ATTRS-config')}"
+                    name="config"
+                    value="{@config_file}"/>
+            <!-- start_opt ~ postmaster_options -->
+            <xsl:if test="postmaster_options">
+            <nvpair id="{concat($Prefix, '-ATTRS-config')}"
+                    name="start_opt"
+                    value="{@postmaster_options}"/>
+            </xsl:if>
+            <!-- pgdba ~ postmaster_user -->
+            <xsl:if test="postmaster_user">
+            <nvpair id="{concat($Prefix, '-ATTRS-config')}"
+                    name="pgdba"
+                    value="{@postmaster_user}"/>
+            </xsl:if>
+            <!-- pgdba ~ postmaster_user -->
+            <xsl:if test="postmaster_user">
+            <nvpair id="{concat($Prefix, '-ATTRS-config')}"
+                    name="pgdba"
+                    value="{@postmaster_user}"/>
+            </xsl:if>
+            <!-- XXX some items from postmaster_options could be
+                 re-parsed into respective RA params
+                 (-h $OCF_RESKEY_pghost) -->
+        </instance_attributes>
+    </xsl:when>
+'''
