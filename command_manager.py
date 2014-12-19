012202f67acfbaa7024e53e0119afb7f73c57b43
Jan Pokorný <jpokorny@redhat.com>
2014-12-19 22:16:37 +0100

    command_manager: increase default width of pretty_cmds
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- command_manager.py
+++ command_manager.py
@@ -132,7 +132,7 @@ class CommandManager(PluginManager):
         #    ec = EC.EXIT_FAILURE
         return ec
 
-    def pretty_cmds(self, text_width=76, linesep_width=1,
+    def pretty_cmds(self, text_width=77, linesep_width=1,
                     ind='  ', itemsep='\n', secsep='\n',
                     cmds_intro='Commands:', aliases_intro='Aliases:',
                     refer_str='alias for {0}'):
