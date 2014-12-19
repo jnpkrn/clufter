3ba44b183e07dc48fba7ad4732c3a54eb9f32b3a
Jan Pokorný <jpokorny@redhat.com>
2014-12-19 20:24:01 +0100

    New ext-plugins/lib-pcs/commands/pcs-rsc-deps
    ...command to output command to install required logical packages
    as per the CIB (known configured resources).
    
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

---
+++ ext-plugins/lib-pcs/commands/pcs_rsc_deps.py
@@ -0,0 +1,39 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2014 Red Hat, Inc.
+# Part of clufter project
+# Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
+"""pcs_resource_deps command"""
+__author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
+
+from ..command import Command
+from ..protocol import protocols
+
+@Command.deco(('pcs-resource-deps',
+                   ('stringlist2stringset',
+                        ('pkgs2distropkgs',
+                            ('pkgs2installcmd')))))
+def pcs_rsc_deps(cmd_ctxt,
+                 input="/var/lib/pacemaker/cib/cib.xml",
+                 output="pcs-resource-deps-{pcs-resource-deps.in.hash}.conf",
+                 install_cmd=False):
+    """Output deps of resources contained in Pacemaker cfg.
+
+    Options:
+        input        input Pacemaker configuration file
+        output       output file with collected dependencies
+        install_cmd  whether to output full install command instead
+    """
+    #if not install_cmd:
+    #    cmd_ctxt['filter_noop'].append('pkgs2installcmd')
+
+    file_proto = protocols.plugins['file'].ensure_proto
+    return (
+        file_proto(input),
+        (
+            (
+                (
+                    file_proto(output),
+                ),
+            ),
+        ),
+    )
