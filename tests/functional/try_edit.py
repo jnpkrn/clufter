560dad4caa26a793560d68e858d602512b80f3dd
Jan Pokorný <jpokorny@redhat.com>
2015-02-18 20:21:43 +0100

    tests: add "functional" test exercising filter/_try_edit
    Also extend .gitignore whitelist appropriately.
    
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

---
+++ tests/functional/try_edit.py
@@ -0,0 +1,38 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2015 Red Hat, Inc.
+# Part of clufter project
+# Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
+"""Test exercising _try_edit"""
+__author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
+
+
+from os.path import join, dirname as d; execfile(join(d(d((__file__))), '_go'))
+
+from os.path import dirname
+from unittest import TestCase
+
+from .filter_manager import FilterManager
+from .utils_prog import getenv_namespaced, setenv_namespaced
+
+
+class TryEdit(TestCase):
+    def setUp(self):
+        setenv_namespaced('NOSALT', 'true')
+        self.flt_mgr = FilterManager.init_lookup('ccs2needlexml',
+                                                 ext_plugins=False)
+
+    #def tearDown(self):
+    #    self.flt_mgr.registry.setup(True)  # start from scratch
+    #    self.flt_mgr = None
+
+    def test_fix_manually(self):
+        testfile = join(dirname(__file__), 'try_edit.conf')
+        out_obj = self.flt_mgr('ccs2needlexml', ('file', testfile),
+                               editor="sed -i -e 's|-1|1|'")
+                               #editor="sed -i 's|\-1|1|;w /dev/stdout' --")
+        #print out_obj('bytestring')
+        with open(join(dirname(__file__), 'try_edit.ok')) as okfd:
+            self.assertEqual(out_obj("bytestring"), okfd.read())
+
+
+from os.path import join, dirname as d; execfile(join(d(d(__file__)), '_gone'))
