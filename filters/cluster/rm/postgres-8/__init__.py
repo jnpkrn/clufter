e6e9ff2b8cf9e2a04006c2d339ba4186648625ed
Jan Pokorný <jpokorny@redhat.com>
2015-03-11 18:13:50 +0100

    filters/ccsflat2pcsprelude[nfsserver]: fix duplicated @pgdba
    Reported-by: Patrik Hagara <phagara@redhat.com>
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- filters/cluster/rm/postgres-8/__init__.py
+++ filters/cluster/rm/postgres-8/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2014 Red Hat, Inc.
+# Copyright 2015 Red Hat, Inc.
 # Part of clufter project
 # Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
 __author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
@@ -34,12 +34,6 @@ ccsflat2pcsprelude = '''\
             <nvpair id="{concat($Prefix, '-ATTRS-config')}"
                     name="start_opt"
                     value="{@postmaster_options}"/>
-            </xsl:if>
-            <!-- pgdba ~ postmaster_user -->
-            <xsl:if test="postmaster_user">
-            <nvpair id="{concat($Prefix, '-ATTRS-config')}"
-                    name="pgdba"
-                    value="{@postmaster_user}"/>
             </xsl:if>
             <!-- pgdba ~ postmaster_user -->
             <xsl:if test="postmaster_user">
