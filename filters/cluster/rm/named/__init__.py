5b2a7af12442f621d72009896082c3b61a87dd63
Jan Pokorný <jpokorny@redhat.com>
2015-04-03 13:20:06 +0200

    filters/ccsflat2cibprelude[named]: enable conversion
    Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=1201278
    Reported-by: Patrik Hagara <phagara@redhat.com>
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

---
+++ filters/cluster/rm/named/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2015 Red Hat, Inc.
+# Part of clufter project
+# Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
+__author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
+
+from ....utils_cib import ResourceSpec, rg2hb_xsl
+
+
+ccsflat2cibprelude = '''\
+    <!--
+        named ~ named
+     -->
+    <xsl:when test="name() = 'named'">
+''' + (
+        ResourceSpec('ocf:heartbeat:named').xsl_attrs
+) + '''
+        <!-- INSTANCE_ATTRIBUTES -->
+        <instance_attributes id="{concat($Prefix, '-ATTRS')}">
+''' + (
+            rg2hb_xsl('named_config', 'config_file')
+            +
+            rg2hb_xsl('named_options', 'named_options')
+) + '''\
+        </instance_attributes>
+
+        <!-- OPERATIONS -->
+        <operations>
+''' + (
+            rg2hb_xsl('stop', 'shutdown_wait', op=True)
+) + '''\
+        </operations>
+    </xsl:when>
+'''
