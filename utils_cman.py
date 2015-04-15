8925ae6c258df81cb64f043ab1ac0898a7efd059
Jan Pokorný <jpokorny@redhat.com>
2015-04-15 13:12:33 +0200

    commands/ccs2pcs+utils_cman: symbolic PATH_CLUSTERCONF
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- utils_cman.py
+++ utils_cman.py
@@ -1,9 +1,11 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2014 Red Hat, Inc.
+# Copyright 2015 Red Hat, Inc.
 # Part of clufter project
 # Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
 """CMAN helpers, mainly used in the filter definitions"""
 __author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
+
+PATH_CLUSTERCONF = '/etc/cluster/cluster.conf'
 
 
 def get_nodes(etree):
