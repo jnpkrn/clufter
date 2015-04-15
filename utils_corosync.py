3fd35a2ff723c35b33eebbac56aa3853724c27f4
Jan Pokorný <jpokorny@redhat.com>
2015-04-15 13:41:27 +0200

    utils_corosync: symbolic PATH_COROCONF
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

---
+++ utils_corosync.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2015 Red Hat, Inc.
+# Part of clufter project
+# Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
+"""CMAN helpers, mainly used in the filter definitions"""
+__author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
+
+PATH_COROCONF = '/etc/corosync/corosync.conf'
