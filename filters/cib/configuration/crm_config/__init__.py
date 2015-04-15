58336233d5336fd066c486702532911e1b6b1602
Jan Pokorný <jpokorny@redhat.com>
2015-04-15 15:01:09 +0200

    filters/cib2pcscmd[crm_config]: handle cluster_property_set
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

---
+++ filters/cib/configuration/crm_config/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2015 Red Hat, Inc.
+# Part of clufter project
+# Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
+__author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
+
+###
+
+from ....utils_xslt import NL
+
+cib2pcscmd = ('''\
+    <xsl:for-each select="cluster_property_set">
+        <xsl:for-each select="nvpair">
+            <!-- unfortunately pcs will throw away the rest of name-value
+                 pairs when first unknown observe; alternatively a single
+                 command with "force" -->
+            <xsl:value-of select="'pcs property set'"/>
+            <xsl:value-of select='" &apos;"'/>
+            <xsl:value-of select="concat(@name, '=', @value)"/>
+            <xsl:value-of select='"&apos;"'/>
+            <xsl:value-of select="'%(NL)s'"/>
+        </xsl:for-each>
+    </xsl:for-each>
+''') % dict(
+    NL=NL
+)
