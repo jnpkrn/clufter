a46c41b192668043802d42c05378afdbcb4dde9d
Jan Pokorný <jpokorny@redhat.com>
2015-05-20 22:03:19 +0200

    filters/cib2pcscmd[constraints]: refactor handling out
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

---
+++ filters/cib/configuration/constraints/rsc_colocation/__init__.py
@@ -0,0 +1,58 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2015 Red Hat, Inc.
+# Part of clufter project
+# Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
+__author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
+
+from ....utils_xslt import NL, xslt_is_member
+
+cib2pcscmd = ('''\
+    <xsl:choose>
+        <!-- plain "with" -->
+        <xsl:when test="@rsc and @with-rsc">
+            <xsl:value-of select="'pcs constraint colocation add'"/>
+            <xsl:if test="
+''' + (
+            xslt_is_member('@rsc-role', ('master', 'slave'))
+) + '''">
+                <xsl:value-of select="concat(' ', @rsc-role)"/>
+            </xsl:if>
+            <xsl:value-of select="concat(' ', @rsc, ' with')"/>
+            <xsl:if test="
+''' + (
+            xslt_is_member('@with-rsc-role', ('master', 'slave'))
+) + '''">
+                <xsl:value-of select="concat(' ', @with-rsc-role)"/>
+            </xsl:if>
+            <xsl:value-of select="concat(' ', @with-rsc)"/>
+            <xsl:if test="@score and not(
+''' + (
+            xslt_is_member('@score', ('INFINITY', '+INFINITY'))
+) + ''')">
+                <xsl:value-of select="concat(' ', @score)"/>
+            </xsl:if>
+            <xsl:value-of select="concat(' ', 'id=', @id)"/>
+            <xsl:choose>
+                <xsl:when test="@score"/>
+                <xsl:when test="@score-attribute">
+                    <xsl:value-of select="concat(' ', 'score-attribute=',
+                                                 @score-attribute)"/>
+                </xsl:when>
+                <xsl:when test="@score-attribute-mangle">
+                    <xsl:value-of select="concat(' ', 'score-attribute-mangle=',
+                                                 @score-attribute-mangle)"/>
+                </xsl:when>
+            </xsl:choose>
+            <xsl:value-of select="'%(NL)s'"/>
+        </xsl:when>
+
+        <!-- resource sets -->
+        <xsl:when test="rsc_colocation[resource_set]">
+            <xsl:message
+            >WARNING: colocation constraint with resource sets not supported (yet)</xsl:message>
+        </xsl:when>
+    </xsl:choose>
+
+''') % dict(
+    NL=NL
+)
---
+++ filters/cib/configuration/constraints/rsc_order/__init__.py
@@ -0,0 +1,41 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2015 Red Hat, Inc.
+# Part of clufter project
+# Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
+__author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
+
+from ....utils_xslt import NL
+
+cib2pcscmd = ('''\
+    <xsl:choose>
+        <!-- plain first-then -->
+        <xsl:when test="@first and @then">
+            <xsl:value-of select="'pcs constraint order'"/>
+            <xsl:if test="@first-action and @first-action != 'start'">
+                <xsl:value-of select="concat(' ', @first-action)"/>
+            </xsl:if>
+            <xsl:value-of select="concat(' ', @first, ' then')"/>
+            <xsl:if test="@then-action and @then-action != 'start'">
+                <xsl:value-of select="concat(' ', @then-action)"/>
+            </xsl:if>
+            <xsl:value-of select="concat(' ', @then)"/>
+            <xsl:value-of select="concat(' ', 'id=', @id)"/>
+            <xsl:if test="@kind">
+                <xsl:value-of select="concat(' ', 'kind=', @kind)"/>
+            </xsl:if>
+            <xsl:if test="@symmetrical">
+                <xsl:value-of select="concat(' ', 'symmetrical=', @symmetrical)"/>
+            </xsl:if>
+            <xsl:value-of select="'%(NL)s'"/>
+        </xsl:when>
+
+        <!-- resource sets -->
+        <xsl:when test="resource_set">
+            <xsl:message
+            >WARNING: order constraint with resource sets not supported (yet)</xsl:message>
+        </xsl:when>
+    </xsl:choose>
+
+''') % dict(
+    NL=NL
+)
