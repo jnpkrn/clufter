436e672768994aff1256bc77b33f842c647b30dd
Jan Pokorný <jpokorny@redhat.com>
2015-05-29 18:22:35 +0200

    filters/ccspcmk2pcscmd: support for "enforcing pcs" (non-default)
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- filters/ccspcmk2pcscmd.py
+++ filters/ccspcmk2pcscmd.py
@@ -19,6 +19,7 @@ def ccspcmk2pcscmd(flt_ctxt, in_obj):
             in_obj,
             textmode=True,
             def_first=xslt_params(
+                pcscmd_force=xslt_boolean(flt_ctxt.get('pcscmd_force', 0)),
                 pcscmd_noauth=xslt_boolean(flt_ctxt.get('pcscmd_noauth', 0)),
                 pcscmd_verbose=xslt_boolean(flt_ctxt.get('pcscmd_verbose', 1)),
                 pcscmd_dryrun=xslt_boolean(flt_ctxt.get('pcscmd_dryrun', 0)),
