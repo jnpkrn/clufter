ed5759308c3ae1e5c477e3f5389eb44e2bea2a3b
Jan Pokorný <jpokorny@redhat.com>
2015-06-19 00:52:11 +0200

    filters/*2pcscmd: drop now-redundant xslt_boolean usage
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- filters/cib2pcscmd.py
+++ filters/cib2pcscmd.py
@@ -7,7 +7,7 @@ __author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
 
 from ..filter import XMLFilter
 from ..utils_xml import squote
-from ..utils_xslt import xslt_boolean, xslt_params
+from ..utils_xslt import xslt_params
 
 TMP_CIB = 'tmp-cib.xml'
 
@@ -15,7 +15,7 @@ TMP_CIB = 'tmp-cib.xml'
 @XMLFilter.deco('cib', 'string-list')
 def cib2pcscmd(flt_ctxt, in_obj):
     """Outputs set of pcs commands to reinstate the cluster per existing CIB"""
-    dry_run = flt_ctxt.get('pcscmd_dryrun', 0)
+    dry_run = flt_ctxt.get('pcscmd_dryrun', False)
     tmp_cib = TMP_CIB if dry_run else flt_ctxt.get('pcscmd_tmpcib', TMP_CIB)
     return (
         'bytestring',
@@ -23,10 +23,10 @@ def cib2pcscmd(flt_ctxt, in_obj):
             in_obj,
             textmode=True,
             def_first=xslt_params(
-                pcscmd_force=xslt_boolean(flt_ctxt.get('pcscmd_force', 0)),
-                pcscmd_verbose=xslt_boolean(flt_ctxt.get('pcscmd_verbose', 1)),
+                pcscmd_force=flt_ctxt.get('pcscmd_force', False),
+                pcscmd_verbose=flt_ctxt.get('pcscmd_verbose', True),
                 pcscmd_tmpcib=squote(tmp_cib),
-                pcscmd_dryrun=xslt_boolean(dry_run),
+                pcscmd_dryrun=dry_run,
                 pcscmd_pcs=squote("pcs -f {0} ".format(tmp_cib)
                                   if tmp_cib else "pcs "),
             ),
