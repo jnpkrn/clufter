6bc26f4666f425db265d3fffe9c69aafcda49133
Jan Pokorný <jpokorny@redhat.com>
2015-06-19 01:08:42 +0200

    filters/*2pcscmd: use unified interface for filter defaults
    (also unit tests benefit from this)
    
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- filters/ccspcmk2pcscmd.py
+++ filters/ccspcmk2pcscmd.py
@@ -9,7 +9,15 @@ from ..filter import XMLFilter
 from ..utils_xslt import xslt_params
 
 
-@XMLFilter.deco('ccs', 'string-list')
+@XMLFilter.deco('ccs', 'string-list', defs=dict(
+    pcscmd_force=False,
+    pcscmd_noauth=False,
+    pcscmd_verbose=True,
+    pcscmd_dryrun=False,
+    pcscmd_enable=False,
+    pcscmd_start_wait=90,
+    pcscmd_noguidance=0,
+))
 def ccspcmk2pcscmd(flt_ctxt, in_obj):
     """Outputs set of pcs commands to reinstate the cluster per cluster.conf"""
     return (
@@ -18,13 +26,13 @@ def ccspcmk2pcscmd(flt_ctxt, in_obj):
             in_obj,
             textmode=True,
             def_first=xslt_params(
-                pcscmd_force=flt_ctxt.get('pcscmd_force', False),
-                pcscmd_noauth=flt_ctxt.get('pcscmd_noauth', False),
-                pcscmd_verbose=flt_ctxt.get('pcscmd_verbose', True),
-                pcscmd_dryrun=flt_ctxt.get('pcscmd_dryrun', False),
-                pcscmd_enable=flt_ctxt.get('pcscmd_enable', False),
-                pcscmd_start_wait=flt_ctxt.get('pcscmd_start_wait', 90),
-                pcscmd_noguidance=flt_ctxt.get('pcscmd_noguidance', False),
+                pcscmd_force=flt_ctxt['pcscmd_force'],
+                pcscmd_noauth=flt_ctxt['pcscmd_noauth'],
+                pcscmd_verbose=flt_ctxt['pcscmd_verbose'],
+                pcscmd_dryrun=flt_ctxt['pcscmd_dryrun'],
+                pcscmd_enable=flt_ctxt['pcscmd_enable'],
+                pcscmd_start_wait=flt_ctxt['pcscmd_start_wait'],
+                pcscmd_noguidance=flt_ctxt['pcscmd_noguidance'],
             ),
         ),
     )
--- filters/cib2pcscmd.py
+++ filters/cib2pcscmd.py
@@ -9,23 +9,27 @@ from ..filter import XMLFilter
 from ..utils_xml import squote
 from ..utils_xslt import xslt_params
 
-TMP_CIB = 'tmp-cib.xml'
 
-
-@XMLFilter.deco('cib', 'string-list')
+@XMLFilter.deco('cib', 'string-list', defs=dict(
+    pcscmd_force=False,
+    pcscmd_verbose=True,
+    pcscmd_tmpcib='tmp-cib.xml',
+    pcscmd_dryrun=False,
+))
 def cib2pcscmd(flt_ctxt, in_obj):
     """Outputs set of pcs commands to reinstate the cluster per existing CIB"""
-    dry_run = flt_ctxt.get('pcscmd_dryrun', False)
-    tmp_cib = TMP_CIB if dry_run else flt_ctxt.get('pcscmd_tmpcib', TMP_CIB)
+    self = flt_ctxt.ctxt_wrapped
+    dry_run, tmp_cib = flt_ctxt['pcscmd_dryrun'], flt_ctxt['pcscmd_tmpcib']
+    tmp_cib = (tmp_cib or self.defs['pcscmd_tmpcib']) if dry_run else tmp_cib
     return (
         'bytestring',
         flt_ctxt.ctxt_proceed_xslt(
             in_obj,
             textmode=True,
             def_first=xslt_params(
-                pcscmd_force=flt_ctxt.get('pcscmd_force', False),
-                pcscmd_verbose=flt_ctxt.get('pcscmd_verbose', True),
-                pcscmd_tmpcib=squote(tmp_cib),
+                pcscmd_force=flt_ctxt['pcscmd_force'],
+                pcscmd_verbose=flt_ctxt['pcscmd_verbose'],
+                pcscmd_tmpcib=squote(flt_ctxt['pcscmd_tmpcib']),
                 pcscmd_dryrun=dry_run,
                 pcscmd_pcs=squote("pcs -f {0} ".format(tmp_cib)
                                   if tmp_cib else "pcs "),
