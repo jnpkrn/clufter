8ccc4d2dcad5f6210d8aefbcc34d6a45df9bf73f
Jan Pokorný <jpokorny@redhat.com>
2015-06-18 18:40:31 +0200

    filters/ccs-version-bump: refactor into separate filter
    Should be stand-alone for easier reusability in the future...
    
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

---
+++ filters/ccs_version_bump.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2015 Red Hat, Inc.
+# Part of clufter project
+# Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
+"""Filter to disable RGManager"""
+__author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
+
+from ..filter import XMLFilter
+
+
+@XMLFilter.deco_xslt('ccs', 'ccs')
+class ccs_version_bump: pass
