b2492bf4d1b9ba85cde5bd5c9225486a4d3dc500
Jan Pokorný <jpokorny@redhat.com>
2015-06-24 23:41:25 +0200

    filters/ccspcmk2pcscmd[cluster]: refactor to shareable template
    (to be reused in needlexml2pcscmd filter)
    
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- filters/_2pcscmd.py
+++ filters/_2pcscmd.py
@@ -40,3 +40,96 @@ def verbose_inform(what):
     verbose_prefix=verbose_prefix or '""',
     NL=NL,
 )
+
+
+def coro2pcscmd(**kwargs):
+    descent = lambda w: \
+        '<clufter:descent-mix at="{what}"/>'.format(
+            what=kwargs.get(w) or w
+        ) if w in kwargs else ''
+    return ('''\
+    <xsl:if test="not($pcscmd_dryrun)">
+        <xsl:if test="not($pcscmd_noauth)">
+''' + (
+            verbose_inform('"auth cluster: ", @name')
+) + '''
+            <xsl:value-of select="'pcs cluster auth'"/>
+
+            %(descent_node)s
+            <xsl:if test="$pcscmd_force">
+                <xsl:value-of select="' --force'"/>
+            </xsl:if>
+            <xsl:value-of select="'%(NL)s'"/>
+''' + (
+            verbose_ec_test
+) + '''
+        </xsl:if>
+        <xsl:if test="not($pcscmd_noguidance)">
+            <!-- see rhbz#1210833 -->
+''' + (
+            verbose_inform('"check cluster includes local machine: ", @name')
+) + r'''
+            <xsl:value-of select="concat(
+                'for l in $(comm -12',
+                    ' &lt;(python -m json.tool /var/lib/pcsd/pcs_users.conf',
+                        ' | sed -n &quot;s|^\s*\&quot;[^\&quot;]\+\&quot;:\s*\&quot;\([0-9a-f-]\+\)\&quot;.*|\1|1p&quot;',
+                        ' | sort)',
+                    ' &lt;(python -m json.tool /var/lib/pcsd/tokens',
+                        ' | sed -n &quot;s|^\s*\&quot;[^\&quot;]\+\&quot;:\s*\&quot;\([0-9a-f-]\+\)\&quot;.*|\1|1p&quot;',
+                        ' | sort)',
+                ') @SENTINEL@; do %(NL)s',
+                'grep -Eq &quot;$(python -m json.tool /var/lib/pcsd/tokens',
+                    ' | sed -n &quot;s|^\s*\&quot;\([^\&quot;]\+\)\&quot;:\s*\&quot;${l}\&quot;.*|\1|1p&quot;)&quot;',
+                    ' - &lt;&lt;&lt;&quot;')"/>
+            %(descent_node)s
+            <xsl:value-of select="concat(
+                '&quot; &amp;&amp; break%(NL)s',
+                'false%(NL)s',
+                'done || {%(NL)s',
+                'echo &quot;WARNING: cluster being created ought to include this very local machine&quot;%(NL)s',
+                'read -p &quot;Do you want to continue [yN] (60s timeout): &quot; -t 60 || :%(NL)s',
+                'test &quot;${REPLY}&quot; = &quot;y&quot; || kill -INT $$%(NL)s',
+                '}%(NL)s:%(NL)s'
+            )"/>
+        </xsl:if>
+''' + (
+        verbose_inform('"new cluster: ", @name')
+) + '''
+        <xsl:value-of select="'pcs cluster setup --start'"/>
+        <xsl:choose>
+            <xsl:when test="$pcscmd_enable">
+                <xsl:value-of select="' --enable'"/>
+            </xsl:when>
+            <xsl:otherwise>
+                <xsl:message>%(msg_enable)s</xsl:message>
+            </xsl:otherwise>
+        </xsl:choose>
+        <xsl:value-of select="concat(' --name ', @name)"/>
+
+        %(descent_node)s
+        %(descent_cman)s
+        %(descent_totem)s
+        %(descent_quorum)s
+        <xsl:value-of select="'%(NL)s'"/>
+''' + (
+        verbose_ec_test
+) + '''
+        <xsl:if test="$pcscmd_start_wait &gt; 0">
+''' + (
+            verbose_inform('"waiting for cluster to come up: ", @name, " seconds"')
+) + '''
+            <xsl:value-of select="concat('sleep ', $pcscmd_start_wait)"/>
+            <xsl:value-of select="'%(NL)s'"/>
+''' + (
+            verbose_ec_test
+) + '''
+        </xsl:if>
+    </xsl:if>
+''') % dict(
+    NL=NL,
+    msg_enable="NOTE: cluster infrastructure services not enabled"
+               " at this point, which can be changed any time by issuing:"
+               " pcs cluster enable --all",
+    **dict(('descent_' + k, descent(k))
+           for k in ('cman', 'node', 'quorum', 'totem'))
+)
