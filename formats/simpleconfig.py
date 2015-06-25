861ca69e14f3bec52b286b6dfdc6ddc1fb7013f5
Jan Pokorný <jpokorny@redhat.com>
2015-06-25 18:45:06 +0200

    formats/simpleconfig: fix missing newline at the end
    (which is implicitly assumed and was present before a rewrite)
    
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- formats/simpleconfig.py
+++ formats/simpleconfig.py
@@ -89,11 +89,11 @@ class simpleconfig(SimpleFormat):
                     ((d - 3) / 2 * indent + x[0] + optsep + x[1], )
                         if isinstance(x, tuple) and len(x) == 2  # dif. SECTION
                     else
-                    # rest
+                    # rest ([''] at the end is for a final newline)
                     ([(d - 3) / 2 * indent + x[0] + lbrace] if d > 1 else [])
                         + list(xxxx for xx in filter(bool, x[1:])
                                for xxx in xx for xxxx in xxx)
-                        + ([(d - 3) / 2 * indent + rbrace] if d > 1 else [])
+                        + ([(d - 3) / 2 * indent + rbrace] if d > 1 else [''])
             )(struct)
         )
         return ret
