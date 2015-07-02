7c70d6c2f898546d215714bf11addef5cf3a7765
Jan Pokorný <jpokorny@redhat.com>
2015-07-02 23:30:33 +0200

    commands/pcs2pcscmd-needle: finalize using new filters
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- commands/pcs2pcscmd.py
+++ commands/pcs2pcscmd.py
@@ -74,25 +74,50 @@ def pcs2pcscmd_flatiron(cmd_ctxt,
 
 
 @Command.deco(('simpleconfig2needlexml',
-                  ('needlexml2pcscmd',
-                      ('stringiter-combine2'))),
+                  ('simpleconfig-normalize',
+                      ('needlexml2pcscmd',
+                          ('stringiter-combine2')))),
               ('cib2pcscmd',
-                      ('stringiter-combine2')))
+                          ('stringiter-combine2')))
 def pcs2pcscmd_needle(cmd_ctxt,
                       coro=PATH_COROCONF,
                       cib=PATH_CIB,
                       output="-",
+                      force=False,
+                      noauth=False,
+                      silent=False,
+                      tmp_cib="{cib2pcscmd.defs[pcscmd_tmpcib]}",
+                      dry_run=False,
+                      enable=False,
+                      start_wait="{needlexml2pcscmd.defs[pcscmd_start_wait]}",
+                      noguidance=False,
+                      text_width='0',
                       _common=XMLFilter.command_common):
-    """[COMMAND CURRENTLY UNAVAILABLE]
+    """(Corosync v2,Pacemaker) cluster cfg. -> reinstating pcs commands
 
     Options:
-        coro      input Corosync v2 config. file
-        cib       input proper Pacemaker cluster config. file
-        output    pcs commands to reinstate the cluster per the inputs
+        coro        input Corosync v2 config. file
+        cib         input proper Pacemaker cluster config. file
+        output      pcs commands to reinstate the cluster per the inputs
+        force       may the force be with emitted pcs commands
+        noauth      skip authentication step (OK if already set up)
+        silent      do not track the progress along the steps execution (echoes)
+        tmp_cib     file to accumulate the changes (empty ~ direct push)
+        dry_run     omit intrusive commands (TMP_CIB reset if empty)
+        enable      enable cluster infrastructure services (autostart on reboot)
+        start_wait  fixed seconds to give cluster to come up initially
+        noguidance  omit extraneous guiding
+        text_width  for commands rewrapping (0/-1/neg. ~ auto/disable/hi-limit)
     """
-    #"""(Corosync v2,Pacemaker) cluster cfg. -> reinstating pcs commands
-
-    raise NotImplementedError("expected to come soon")
+    cmd_ctxt['pcscmd_force'] = force
+    cmd_ctxt['pcscmd_noauth'] = noauth
+    cmd_ctxt['pcscmd_verbose'] = not(silent)
+    cmd_ctxt['pcscmd_tmpcib'] = tmp_cib
+    cmd_ctxt['pcscmd_dryrun'] = dry_run
+    cmd_ctxt['pcscmd_enable'] = enable
+    cmd_ctxt['pcscmd_start_wait'] = start_wait
+    cmd_ctxt['pcscmd_noguidance'] = noguidance
+    cmd_ctxt['text_width'] = text_width
 
     file_proto = protocols.plugins['file'].ensure_proto
     return (
@@ -100,12 +125,14 @@ def pcs2pcscmd_needle(cmd_ctxt,
             file_proto(coro),
             (
                 (
-                    file_proto(coro),
+                    (
+                        file_proto(output),
+                    ),
                 ),
             ),
            file_proto(cib),
            #(
-           #    file_proto(output),  # already tracked
+           #            file_proto(output),  # already tracked
            #),
         ),
     )
