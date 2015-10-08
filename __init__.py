cbe1207c8ef90d848089a1d68f21cba6c647a409
Jan Pokorný <jpokorny@redhat.com>
2015-10-08 23:04:32 +0200

    Bump version to 0.50.5
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-version, alpha = '0.50.5', True
+version, alpha = '0.50.5', False
 
 # https://www.python.org/dev/peps/pep-0440 + git export magic using export-subst
 _git_hash = "$Format:%h$".strip('$').replace("Format:%h", "")
