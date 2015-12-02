3f3d004991493c4906a2eb9996a3c3d29c65d3ff
Jan Pokorný <jpokorny@redhat.com>
2015-12-02 17:10:13 +0100

    filters/ccs2needlexml[logging_daemon]: attrs from logging + subsys
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- filters/cluster/logging/logging_daemon/__init__.py
+++ filters/cluster/logging/logging_daemon/__init__.py
@@ -1,23 +1,24 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2014 Red Hat, Inc.
+# Copyright 2015 Red Hat, Inc.
 # Part of clufter project
 # Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
 
 from ....utils_xslt import xslt_is_member
 
-###
+from logging import getLogger
+log = getLogger(__name__)
 
-# XXX: logging/__init__.py: ccs2needlexml_attrs + ('subsys', )
-ccs2needlexml_attrs = (
-    'debug',
-    'logfile',
-    'logfile_priority',
-    'subsys',
-    'syslog_facility',
-    'syslog_priority',
-    'to_logfile',
-    'to_syslog',
-)
+# XXX a bit dirty DRY approach
+from os.path import dirname, join
+use = join(reduce(lambda a, b: dirname(a), xrange(2), __file__), '__init__.py')
+myglobals = dict(__package__=__package__, __name__=__name__)
+try:
+    execfile(use, myglobals)
+except IOError:
+    log.error("Unable to refer to `{0}' file".format(use))
+    ccs2needlexml_attrs = None  # make it fail later on if ccs2needlexml used
+else:
+    ccs2needlexml_attrs = myglobals['ccs2needlexml_attrs'] + ('subsys', )
 
 ccs2needlexml = '''\
     <xsl:if test="@name='corosync' and @subsys">
