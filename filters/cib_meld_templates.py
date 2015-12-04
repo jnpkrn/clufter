341f5c1af1749fe5ee379a4e0c8c96902bd61df5
Jan Pokorný <jpokorny@redhat.com>
2015-12-04 23:01:02 +0100

    filters/cib-meld-templates: compensate pcs template incompat.
    See <https://bugzilla.redhat.com/1281359>.
    
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

---
+++ filters/cib_meld_templates.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2015 Red Hat, Inc.
+# Part of clufter project
+# Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
+"""cib-meld-templates filter"""
+__author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
+
+from ..filter import XMLFilter
+
+
+@XMLFilter.deco_xslt('cib', 'cib')
+class cib_meld_templates: pass
