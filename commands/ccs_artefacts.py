5a29d61107bc355384ba0214b7d7bed867a4a219
Jan Pokorný <jpokorny@redhat.com>
2015-12-09 20:28:10 +0100

    commands + filters: fix minor discrepancies (comments, ...)
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- commands/ccs_artefacts.py
+++ commands/ccs_artefacts.py
@@ -1,8 +1,8 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2014 Red Hat, Inc.
+# Copyright 2015 Red Hat, Inc.
 # Part of clufter project
 # Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
-"""cluster-artefacts command"""
+"""ccs-artefacts command"""
 __author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
 
 from ..command import Command
--- filters/ccs_obfuscate.py
+++ filters/ccs_obfuscate.py
@@ -1,8 +1,8 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2014 Red Hat, Inc.
+# Copyright 2015 Red Hat, Inc.
 # Part of clufter project
 # Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
-"""obfuscation filters for ccs"""
+"""Obfuscation filters for ccs"""
 __author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
 
 from ..filter import XMLFilter
