81831cadbdb947917f234d953ee20ac9f450339c
Jan Pokorný <jpokorny@redhat.com>
2015-12-10 16:03:53 +0100

    filters/cib2pcscmd[{op,rsc}_defaults]: handle defaults as well
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

---
+++ filters/cib/configuration/op_defaults/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2015 Red Hat, Inc.
+# Part of clufter project
+# Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
+__author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
+
+###
+
+from ....filters._2pcscmd import verbose_ec_test, verbose_inform
+from ....utils_xslt import NL
+
+
+cib2pcscmd = ('''\
+    <xsl:if test="meta_attributes/nvpair">
+''' + (
+        verbose_inform('"set operation defaults"')
+) + '''
+        <xsl:value-of select='concat($pcscmd_pcs, "resource op defaults")'/>
+        <xsl:for-each select="meta_attributes">
+            <xsl:for-each select="nvpair">
+                <xsl:value-of select='concat(" &apos;", @name, "=", @value, "&apos;")'/>
+            </xsl:for-each>
+        </xsl:for-each>
+        <xsl:value-of select="'%(NL)s'"/>
+''' + (
+        verbose_ec_test
+) + '''
+    </xsl:if>
+''') % dict(
+    NL=NL,
+)
