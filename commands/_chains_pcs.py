ddd20a9d067676cf7cfd3f6ea074b60561dde901
Jan Pokorný <jpokorny@redhat.com>
2015-12-16 18:35:27 +0100

    commands: fix inner logic of chains wrt. singular tail
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- commands/_chains_pcs.py
+++ commands/_chains_pcs.py
@@ -5,7 +5,7 @@
 """Chains of filters used in *2pcs* commands"""
 __author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
 
-from ..utils import args2unwrapped, tuplist
+from ..utils import args2sgpl, args2unwrapped, tuplist
 from ..utils_func import apply_aggregation_preserving_passing_depth
 
 terminalize = lambda args: \
@@ -30,14 +30,14 @@ ccsflat2cibfinal_chain_exec = lambda cont=(): \
         ('ccsflat2cibprelude',
             ('cibprelude2cibcompact',
                 ('cibcompact2cib',
-                    args2unwrapped('cib2cibfinal', *cont)))))
+                    (args2unwrapped('cib2cibfinal', *args2sgpl(cont)))))))
 ccsflat2cibfinal_chain = ccsflat2cibfinal_chain_exec()
 ccsflat2cibfinal_output = cast_output(ccsflat2cibfinal_chain)
 
 cib2pcscmd_chain_exec = lambda cont=(): \
     ('cib-revitalize',
         ('cib-meld-templates',
-            args2unwrapped('cib2pcscmd', *cont)))
+            (args2unwrapped('cib2pcscmd', *args2sgpl(cont)))))
 cib2pcscmd_chain = cib2pcscmd_chain_exec()
 cib2pcscmd_output = cast_output(cib2pcscmd_chain)
 
