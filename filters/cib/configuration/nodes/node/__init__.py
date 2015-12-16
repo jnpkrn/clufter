57e78411d1f57e3a7247360ab1d971645021859a
Jan Pokorný <jpokorny@redhat.com>
2015-12-16 19:05:23 +0100

    filters/cib2pcscmd[configuration/*]: deploy attrset_xsl "macro"
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- filters/cib/configuration/nodes/node/__init__.py
+++ filters/cib/configuration/nodes/node/__init__.py
@@ -7,6 +7,7 @@ __author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
 ###
 
 from ....filters._2pcscmd import verbose_ec_test, verbose_inform
+from ....filters.cib2pcscmd import attrset_xsl
 from ....utils_xslt import NL
 
 cib2pcscmd = ('''\
@@ -16,11 +17,9 @@ cib2pcscmd = ('''\
 ) + '''
         <xsl:value-of select='concat($pcscmd_pcs, "property set")'/>
         <xsl:value-of select="concat(' --node ', @uname)"/>
-        <xsl:for-each select="instance_attributes">
-            <xsl:for-each select="nvpair">
-                <xsl:value-of select='concat(" &apos;", @name, "=", @value, "&apos;")'/>
-            </xsl:for-each>
-        </xsl:for-each>
+''' + (
+        attrset_xsl("instance_attributes")
+) + '''
         <xsl:value-of select="'%(NL)s'"/>
 ''' + (
         verbose_ec_test
