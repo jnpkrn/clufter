73d3ed666d4ade1b2be24096f4bad83f40c349ab
Jan Pokorný <jpokorny@redhat.com>
2016-01-22 17:04:11 +0100

    filters/cib2pcscmd[...]: simplify using new attrset_xml
    Fix a run-check test regression along.
    
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- filters/cib/configuration/op_defaults/__init__.py
+++ filters/cib/configuration/op_defaults/__init__.py
@@ -1,30 +1,16 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2015 Red Hat, Inc.
+# Copyright 2016 Red Hat, Inc.
 # Part of clufter project
 # Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
 __author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
 
 ###
 
-from ....filters._2pcscmd import verbose_ec_test, verbose_inform
 from ....filters.cib2pcscmd import attrset_xsl
-from ....utils_xslt import NL
 
 
-cib2pcscmd = ('''\
-    <xsl:if test="meta_attributes/nvpair">
-''' + (
-        verbose_inform('"set operation defaults"')
-) + '''
-        <xsl:value-of select='concat($pcscmd_pcs, "resource op defaults")'/>
-''' + (
-        attrset_xsl("meta_attributes")
-) + '''
-        <xsl:value-of select="'%(NL)s'"/>
-''' + (
-        verbose_ec_test
-) + '''
-    </xsl:if>
-''') % dict(
-    NL=NL,
+cib2pcscmd = (
+    attrset_xsl("meta_attributes",
+                cmd='$pcscmd_pcs, "resource op defaults"',
+                inform='"set operation defaults"')
 )
