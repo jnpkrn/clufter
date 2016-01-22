2fd302a570c1188b84dcb93ab486d30b6d391379
Jan Pokorný <jpokorny@redhat.com>
2016-01-22 14:58:27 +0100

    filters/cib2pcscmd[group]: fix not handling group meta attrs
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- filters/cib/configuration/resources/group/__init__.py
+++ filters/cib/configuration/resources/group/__init__.py
@@ -1,10 +1,11 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2015 Red Hat, Inc.
+# Copyright 2016 Red Hat, Inc.
 # Part of clufter project
 # Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
 __author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
 
 from ....filters._2pcscmd import verbose_ec_test, verbose_inform
+from ....filters.cib2pcscmd import attrset_xsl
 from ....utils_xslt import NL
 
 cib2pcscmd = ('''\
@@ -19,6 +20,21 @@ cib2pcscmd = ('''\
 ''' + (
     verbose_ec_test
 ) + '''
+
+    <!-- meta attrs -->
+    <xsl:if test="meta_attributes/nvpair">
+''' + (
+        verbose_inform('"meta attributes for group: ", @id')
+) + '''
+        <xsl:value-of select="concat($pcscmd_pcs, 'resource meta ', @id)"/>
+''' + (
+            attrset_xsl("meta_attributes")
+) + '''
+        <xsl:value-of select="'%(NL)s'"/>
+''' + (
+        verbose_ec_test
+) + '''
+    </xsl:if>
 ''') % dict(
     NL=NL,
 )
