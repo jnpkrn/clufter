eeb3baa8119383ee28da703721263e655716d4f2
Jan Pokorný <jpokorny@redhat.com>
2016-02-01 22:09:44 +0100

    filter+format: harden (+prepare grounds for) XML/XSLT usage
    Forbid any direct network/disk side-effects for XSLT and make sure we
    are using XML parser without network access.
    
    In the future, there is an possibility to forbid also entities
    replacement should it be considered risky and unintrusive.
    
    The new lxml.etree wrappers are placed into new utils_lxml module.
    
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

---
+++ utils_lxml.py
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2016 Red Hat, Inc.
+# Part of clufter project
+# Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
+"""Wrapper around standard lxml.etree static methods"""
+__author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
+
+from lxml import etree
+
+etree_XSLT_safe = lambda _input, **kwargs: \
+    etree.XSLT(_input,
+               **dict(access_control=etree.XSLTAccessControl.DENY_ALL,
+                      **kwargs))
+
+etree_parser_safe_kwargs = dict(
+    no_network=True,
+    #resolve_entities=False,
+)
+
+etree_parser_safe = etree.XMLParser(**etree_parser_safe_kwargs)
