a018eb78f55148ec651d439e6ef5cd92bcf2075f
Jan Pokorný <jpokorny@redhat.com>
2016-07-15 14:40:14 +0200

    filters/needleqdevicexml2pcscmd: new filter + unit test
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

---
+++ filters/corosync/quorum/device/net/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2016 Red Hat, Inc.
+# Part of clufter project
+# Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
+
+###
+
+from ....filters._2pcscmd import verbose_ec_test, verbose_inform
+from ....utils_xslt import NL, xslt_is_member, xslt_string_mapping
+
+
+needlexml2pcscmd_attrs = (
+    'algorithm',
+    'connect_timeout',
+    'force_ip_version',
+    'host',
+    'port',
+    'tie_breaker',
+    'tls',
+)
+
+needleqdevicexml2pcscmd = '''\
+    <xsl:when test="@model = 'net'">
+        <xsl:for-each select="net/@*[
+''' + (
+            xslt_is_member('name()', needlexml2pcscmd_attrs)
+) + ''']">
+            <xsl:value-of select="concat(' ', name(), '=', .)"/>
+        </xsl:for-each>
+    </xsl:when>
+'''
---
+++ filters/needleqdevicexml2pcscmd.py
@@ -0,0 +1,30 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2016 Red Hat, Inc.
+# Part of clufter project
+# Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
+"""needleqdevicexml2pcscmd filter"""
+__author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
+
+from ..filter import XMLFilter
+from ..utils_xslt import xslt_params
+
+
+@XMLFilter.deco('coroxml-needle', 'string-list', defs=dict(
+    pcscmd_force=False,
+    pcscmd_verbose=True,
+    pcscmd_noguidance=0,
+))
+def needleqdevicexml2pcscmd(flt_ctxt, in_obj):
+    """Outputs set of pcs commands to reinstate the qdevice per corosync.conf"""
+    return (
+        'bytestring',
+        flt_ctxt.ctxt_proceed_xslt(
+            in_obj,
+            textmode=True,
+            def_first=xslt_params(
+                pcscmd_force=flt_ctxt['pcscmd_force'],
+                pcscmd_verbose=flt_ctxt['pcscmd_verbose'],
+                pcscmd_noguidance=flt_ctxt['pcscmd_noguidance'],
+            ),
+        ),
+    )
