d1d82a9b8478ddcdbb3147646ecd5999665d5354
Jan Pokorný <jpokorny@redhat.com>
2016-07-15 14:40:14 +0200

    filters/needleqdevicexml2pcscmd: new filter + unit test
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

---
+++ filters/corosync/quorum/device/__init__.py
@@ -0,0 +1,61 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2016 Red Hat, Inc.
+# Part of clufter project
+# Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
+
+###
+
+from ....filters._2pcscmd import verbose_ec_test, verbose_inform
+from ....utils_xslt import NL, xslt_is_member, xslt_string_mapping
+
+needlexml2pcscmd_attrs = (
+    # model has a special role here
+    'sync_timeout',
+    'timeout',
+    'votes',
+)
+
+needleqdevicexml2pcscmd = ('''\
+''' + (
+    verbose_inform('"add quorum device: ", @model, " model"')
+) + '''
+    <xsl:value-of select="concat('pcs quorum add --help',
+                                 ' &gt;/dev/null 2&gt;&amp;1 &amp;&amp;',
+                                 ' pcs quorum add')"/>
+
+    <xsl:for-each select="@*[
+''' + (
+        xslt_is_member('name()', needlexml2pcscmd_attrs)
+) + ''']">
+        <xsl:value-of select="concat(' ', name(), '=', .)"/>
+    </xsl:for-each>
+
+    <xsl:value-of select="concat(' model ', @model)"/>
+
+    <xsl:choose>
+
+        <!-- make sure there is at least a single "when" -->
+        <xsl:when test="false()"/>
+
+        <!-- the nested snippets should be guarded with
+             xsl:when test="@model = 'foo'" -->
+        <clufter:descent-mix at="*"/>
+
+        <xsl:otherwise>
+            <xsl:comment> %(note_unhandled)s </xsl:comment>
+            <xsl:message>%(note_unhandled)s</xsl:message>
+        </xsl:otherwise>
+    </xsl:choose>
+
+    <xsl:value-of select="' || :'"/>
+    <xsl:value-of select="'%(NL)s'"/>
+''' + (
+    verbose_ec_test
+) + '''
+''') % dict(
+    NL=NL,
+    note_unhandled='''<xsl:value-of select="concat('WARNING: quorum device model `',
+                                                   @model,
+                                                   '` is currently unhandled',
+                                                   ' in the conversion')"/>'''
+)
---
+++ tests/filters/needleqdevicexml2pcscmd.py
@@ -0,0 +1,39 @@
+# -*- coding: UTF-8 -*-
+# Copyright 2016 Red Hat, Inc.
+# Part of clufter project
+# Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
+"""Testing `needlexml2pcscmd' filter"""
+__author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
+
+# following makes available also: TeardownFilterTestCase, rewrite_root
+from os.path import join, dirname as d; execfile(join(d(d((__file__))), '_com'))
+
+
+from .filter_manager import FilterManager
+flt = 'needleqdevicexml2pcscmd'
+cib2pcscmd = FilterManager.init_lookup(flt).filters[flt]
+cib = cib2pcscmd.in_format
+
+
+class FiltersCib2pcscmdQuorumDeviceTestCase(TeardownFilterTestCase):
+    def testQuorumDeviceNet(self):
+        flt_obj = rewrite_root(self.flt_mgr.filters[flt], 'corosync/quorum')
+        in_fmt = flt_obj.in_format
+        io_strings = (
+            ('''\
+<device model="net">
+    <net host="191.168.10.20"/>
+</device>
+''', '''\
+pcs quorum add --help >/dev/null 2>&1 && pcs quorum add model net host=191.168.10.20 || :
+'''),
+        )
+        for (in_str, out_str) in io_strings:
+            in_obj = in_fmt('bytestring', in_str,
+                            validator_specs={in_fmt.ETREE: ''})
+            out_obj = flt_obj(in_obj, pcscmd_verbose=False, pcscmd_tmpcib='')
+            #print out_obj.BYTESTRING()
+            self.assertEquals(out_obj.BYTESTRING(), out_str)
+
+
+from os.path import join, dirname as d; execfile(join(d(d(__file__)), '_gone'))
