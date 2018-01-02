83b3d32598637e87b2787ee19ee893cc9e048a4e
Jan Pokorný <jpokorny@redhat.com>
2018-01-02 18:03:49 +0100

    filters/needlexml2pcscmd[node]: nodelist.node.name not supported by pcs
    Related-to: <https://bugzilla.redhat.com/1517834>
    Signed-off-by: Jan Pokorný <jpokorny@redhat.com>

--- filters/corosync/nodelist/node/__init__.py
+++ filters/corosync/nodelist/node/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2015 Red Hat, Inc.
+# Copyright 2018 Red Hat, Inc.
 # Part of clufter project
 # Licensed under GPLv2+ (a copy included | http://gnu.org/licenses/gpl-2.0.txt)
-
+__author__ = "Jan Pokorný <jpokorny @at@ Red Hat .dot. com>"
 
 ###
 
@@ -10,5 +10,14 @@ needlexml2pcscmd = '''\
     <xsl:value-of select="concat(' ', @ring0_addr)"/>
     <xsl:if test="@ring1_addr">
         <xsl:value-of select="concat(',', @ring1_addr)"/>
+    </xsl:if>
+    <xsl:if test="@name">
+        <!-- https://bugzilla.redhat.com/1183103 -->
+        <xsl:message>
+            <xsl:value-of select="concat('WARNING: `nodelist.node.name`',
+                                         ' value `', ., '` specified,',
+                                         ' but current pcs not capable',
+                                         ' to set it')"/>
+        </xsl:message>
     </xsl:if>
 '''
